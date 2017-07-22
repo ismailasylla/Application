ba4a993ed1f7ab32586efc68190c9c3a78f52b62
ismaila sylla <syldox@gmail.com>
2017-07-22 18:09:50 +0530

    18-Populating the user roles select

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,24 +8,49 @@
 <h1>Create Users</h1>
 
 
-            {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+    {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
 
-                    <div class="form-group">
-
-                        {!! Form::label('title','Title:') !!}
-                        {!! Form::text('title', null, ['class'=>'form-control']) !!}
-
-                    </div>
+            <div class="form-group">
+                {!! Form::label('name','Name:') !!}
+                {!! Form::text('name', null, ['class'=>'form-control']) !!}
+            </div>
 
 
-                    <div class="form-group">
+            <div class="form-group">
+                {!! Form::label('email','Email:') !!}
+                {!! Form::email('email', null, ['class'=>'form-control']) !!}
+            </div>
 
-                        {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
 
-                    </div>
+            <div class="form-group">
+                {!! Form::label('role_id','Role:') !!}
+                {!! Form::select('role_id', [''=>'Choose Options'] + $roles , null, ['class'=>'form-control']) !!}
+            </div>
 
 
-            {!! Form::close() !!}
+            <div class="form-group">
+
+                {!! Form::label('status','Status:') !!}
+                {!! Form::select('status', array(1 =>'Active', 0=>'Not Active'), null, ['class'=>'form-control']) !!}
+
+            </div>
+
+
+                <div class="form-group">
+
+                                {!! Form::label('password','Password:') !!}
+                                {!! Form::password('password', ['class'=>'form-control']) !!}
+
+                </div>
+
+
+
+            <div class="form-group">
+                {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+            </div>
+
+
+    {!! Form::close() !!}
 
 
 
