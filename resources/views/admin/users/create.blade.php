71321f1eedae96f294e8c9d31b335736496047f3
ismaila sylla <syldox@gmail.com>
2017-07-22 06:15:46 +0530

    creating form for user create 1

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -8,4 +8,26 @@
 <h1>Create Users</h1>
 
 
+            {!! Form::open(['method'=>'POST','action'=>'AdminUsersController@store']) !!}
+
+                    <div class="form-group">
+
+                        {!! Form::label('title','Title:') !!}
+                        {!! Form::text('title', null, ['class'=>'form-control']) !!}
+
+                    </div>
+
+
+                    <div class="form-group">
+
+                        {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
+
+                    </div>
+
+
+            {!! Form::close() !!}
+
+
+
+
 @endsection
